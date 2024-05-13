c45049dc26c5ab562cf1ecb8accdfc559c2a3038
Alex Bons <36956@ma-web.nl>
2024-05-13 10:38:17 +0200

    06

---
+++ ListOpdracht/ListOpdracht/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> birds = new List<string>{"uil", "kraai" ,"hond" ,"papegaai"};
+            List<string> birds2 = new List<string> { "meeuw", "duif" };
+
+            birds2.AddRange(birds);
+
+            birds2.RemoveAt(4);
+            foreach (string i in birds2)
+            {
+                Console.WriteLine(i);
+            }
+          
+        }
+
+
+
+    }
+}
