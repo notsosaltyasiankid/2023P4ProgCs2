9a17238b615c953aae93869fb5c4c3093c2df49f
Alex Bons <36956@ma-web.nl>
2024-05-13 09:53:13 +0200

    04

--- ConsoleMonGame/ConsoleMonGame/Skill.cs
+++ ConsoleMonGame/ConsoleMonGame/Skill.cs
@@ -5,5 +5,13 @@
         internal int damage;
         internal int energyCost;
         internal string name;
+
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+        
+        
     }
 }
